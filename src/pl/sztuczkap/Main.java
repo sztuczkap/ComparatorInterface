609052a1308339074d758f6278e5d2ac3426a50c
sztuczkap <p.sztuczka@wp.pl>
2020-12-28 23:34:04 +0100

    interfejs Comparator

--- src/pl/sztuczkap/Main.java
+++ src/pl/sztuczkap/Main.java
@@ -1,8 +1,26 @@
 package pl.sztuczkap;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee("Jan", 40, 5000.50));
+        employees.add(new Employee("Adam", 30, 3900.20));
+        employees.add(new Employee("Adam", 30, 2800));
+        employees.add(new Employee("Adam", 22, 3800.99));
+        employees.add(new Employee("Monika", 35, 9000));
+        employees.add(null);
+
+        Comparator<Employee> comparator = Comparator.comparing(Employee::getName) //(e1, e2) -> e1.getName().compareTo(e2.getName());
+                .thenComparingInt(Employee::getAge)
+                .thenComparingDouble(Employee::getSalary)
+                .reversed();
+        employees.sort(Comparator.nullsLast(comparator));
+        employees.forEach(System.out::println);
     }
 }
